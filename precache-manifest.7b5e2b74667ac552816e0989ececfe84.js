f031ef74cb662e83b088b3683dc642d570561978
vikash990 <vikashadubey>
2020-07-03 18:35:22 +0530

    Updates

---
+++ precache-manifest.7b5e2b74667ac552816e0989ececfe84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cebd1aa9fdec516dbc561c65db3bf5",
+    "url": "/react-deployment/index.html"
+  },
+  {
+    "revision": "5618fb00b28fbf77cbac",
+    "url": "/react-deployment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "950d6a2e06dfc33329e2",
+    "url": "/react-deployment/static/js/2.8d92efea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deployment/static/js/2.8d92efea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5618fb00b28fbf77cbac",
+    "url": "/react-deployment/static/js/main.868004f1.chunk.js"
+  },
+  {
+    "revision": "a562f7695bf17bdb144d",
+    "url": "/react-deployment/static/js/runtime-main.d5dc4cc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
